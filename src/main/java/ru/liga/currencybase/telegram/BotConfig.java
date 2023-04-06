7a97ac1a76a55771b181fde51cac122c4cb07f51
Facnet <zabuza53@yandex.ru>
2023-04-06 17:07:26 +0300

    refactor?

---
+++ src/main/java/ru/liga/currencybase/telegram/BotConfig.java
@@ -0,0 +1,6 @@
+package ru.liga.currencybase.telegram;
+
+public class BotConfig {
+    public static final String CURRENCY_BASE_TOKEN = "[TOP_SECRET_TOKEN]";
+    public static final String CURRENCY_BASE_USER = "nameProjectBot";
+}
